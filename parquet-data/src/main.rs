53c3de20e1d897459a609cbd82a457f8e3616f47
jkarasha <jkarasha@gmail.com>
2023-01-14 21:12:28 -0800

    Add code to read from parquet file.

--- parquet-data/src/main.rs
+++ parquet-data/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use datafusion::error::Result;
+use datafusion::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let context = SessionContext::new();
+    let test_data = datafusion::test_util::parquet_test_data();
+
+    context.register_parquet(
+        "alltypes_plain",
+        &format!("{test_data}/alltypes_plain.parquet"),
+        ParquetReadOptions::default(),
+    ).await?;
+
+    let df = context.sql(
+        "select * from alltypes_plain"
+    ).await?;
+
+    df.show().await?;
+
+    Ok(())
 }
