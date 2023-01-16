2a5d4a03ed511d46a1e0852171b8b28f480b4308
jkarasha <jkarasha@gmail.com>
2023-01-15 19:36:27 -0800

    Use hard-coded data folder.

--- csv-data/src/main.rs
+++ csv-data/src/main.rs
@@ -5,16 +5,16 @@ use datafusion::prelude::*;
 async fn main() -> Result<()> {
     let context = SessionContext::new();
     //let test_data = datafusion::test_util::arrow_test_data();
-    let test_data = "../testing/data";
+    let test_data = "/mnt/c/Dev/Rust/archive";
     //
     context.register_csv(
-        "aggregate_test_1oo",
-        &format!("{test_data}/csv/bing_covid-19_data.csv"),
+        "oj_data",
+        &format!("{test_data}/oj.csv"),
         CsvReadOptions::new(),
     ).await?;
 
     let df = context.sql(
-        "select * from bing_covid-19_data"
+        "select * from oj_data"
     ).await?;
 
     df.show().await?;
